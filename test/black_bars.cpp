2dad8d31e3ccecce03aa6601c72a82caab96aac0
Raini Wu <rainiwu01@gmail.com>
2020-06-23 11:15:08 -0700

    completing the black bars test

--- test/black_bars.cpp
+++ test/black_bars.cpp
@@ -8,13 +8,42 @@
 #define WIDTH 6144
 #define HEIGHT 4608
 #define FPS 60
-#define TP 0.4
 // set REVERT to 1 and recompile for TSP-->CMP conversion
 #define REVERT 1
+#define TP 0.1
 
-int main(void) {
-	VideoCapture source_video("../media/balboa_cmp_trunc2.mkv");
+#define ROW 1
+#define COL 0
+#define NUM 1
+
+// enum Face {TOP, LEFT, BASE, RIGHT, BACK, BOTTOM};
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	VideoCapture source_video(argv[1]);
 	int ex = static_cast<int>(source_video.get(CAP_PROP_FOURCC));
 	Size t = Size(WIDTH, HEIGHT);
+	
+	int face_size = HEIGHT / 3;
+	Mat src, dst;
+	Mat face;
+
+	//Mat map[2][3];
+	Mat end_faces[6]; 
+	
+	source_video >> src;
+	src(Rect(face_size*COL, face_size*ROW, face_size, face_size)).copyTo(face); // roi extraction
+	tspform((Face) NUM, face, TP, dst, REVERT);
+
+	if(REVERT) imwrite("revert.png", dst);
+	else imwrite("output.png", dst);
+
+	namedWindow( "Display Image", WINDOW_AUTOSIZE );
+	imshow( "Display Image", dst); 
+
+	waitKey(0);
+	return 0;	
 
 }
